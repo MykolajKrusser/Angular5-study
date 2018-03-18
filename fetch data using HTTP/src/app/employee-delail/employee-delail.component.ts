0ce8aeb2819165ee8f0f78f6276774f0cae03b34
Mykolaj <32041427+MykolajKrusser@users.noreply.github.com>
2018-03-18 17:03:07 +0100

    Http requests in Angular lesson

---
+++ fetch data using HTTP/src/app/employee-delail/employee-delail.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {EmployeeService} from "../employee.service";
+
+@Component({
+  selector: 'app-employee-delail',
+  templateUrl: './employee-delail.component.html',
+  styleUrls: ['./employee-delail.component.css']
+})
+export class EmployeeDelailComponent implements OnInit {
+  public employees = [];
+  constructor(private _employeeService: EmployeeService) { }
+
+  ngOnInit() {
+    this._employeeService.getEmployees()
+        .subscribe(data => this.employees = data);
+  }
+
+}
